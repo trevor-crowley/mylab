90f2f50d00b01483e7757c8916931376098d1e8b
trevor-crowley <trevor_crowley@hotmail.com>
2018-01-28 23:53:07 -0500

    init

--- utilities/get_scope_data.m
+++ utilities/get_scope_data.m
@@ -1,18 +1,19 @@
 %% Instrument Connection
 
 % Find a tcpip object.
-obj1 = instrfind('Type', 'tcpip', 'RemoteHost', '192.168.2.108', 'RemotePort', 5555, 'Tag', '');
+obj1 = instrfind('Type', 'tcpip', 'RemoteHost', getenv('IP_RIGOL_SCOPE'), 'RemotePort', 5555, 'Tag', '');
 
 % Create the tcpip object if it does not exist
 % otherwise use the object that was found.
 if isempty(obj1)
-    obj1 = tcpip('192.168.2.108', 5555);
+    obj1 = tcpip(getenv('IP_RIGOL_SCOPE'), 5555);
 else
     fclose(obj1);
     obj1 = obj1(1);
 end
 
-set(obj1, 'InputBufferSize', 5600011);
+set(obj1, 'InputBufferSize', 14000011);
+% set(obj1, 'InputBufferSize', 5600011);
 set(obj1, 'Timeout', 10);
 
 % Connect to instrument object, obj1.
@@ -46,10 +47,12 @@ fscanf(obj1, '%s\n')
 
 %% Set Mode
 
+num_points  = 1400000;
+
 fprintf(obj1, ':STOP');
 fprintf(obj1, ':WAV:SOUR CHAN1');
 fprintf(obj1, ':WAV:MODE RAW');
-fprintf(obj1, ':WAV:POIN 1400000');
+fprintf(obj1, ':WAV:POIN %d', num_points);
 
 fprintf(obj1, ':WAV:RES');
 fprintf(obj1, ':WAV:BEG');
@@ -62,7 +65,7 @@ fscanf(obj1, '%s\n')
 %% Get data1
 tic;
 fprintf(obj1, ':WAV:DATA?');
-[data1, len] = fread(obj1,1400000+11,'uint8');
+[data1, len] = fread(obj1,num_points+11,'uint8');
 fprintf(obj1, ':WAV:END');
 toc;
 
@@ -72,7 +75,7 @@ toc;
 wave = data1(12:end)';
 subplot(211);
 plot(wave);
-fftSpec = fft(wave',1400000);
+fftSpec = fft(wave',num_points);
 fftRms = abs( fftSpec');
 fftLg = 20*log(fftRms);
 subplot(212);
