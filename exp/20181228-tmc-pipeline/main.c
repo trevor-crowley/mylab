f17b248ccd5a1c2a56b8addae6348af62315febf
trevor <trevor@luigilinux.local>
2019-01-12 17:27:25 -0500

    init

---
+++ exp/20181228-tmc-pipeline/main.c
@@ -0,0 +1,12 @@
+
+
+int main() {
+	int i;
+	float val=0;
+
+	for(i=0; i<10; i++) {
+		val += i;
+
+	}
+	return 0;
+}
---
+++ exp/20181228-tmc-pipeline/pipeline/solution1/sdx.tcl
@@ -0,0 +1,10 @@
+# ==============================================================
+# File generated on Fri Dec 28 13:18:57 EST 2018
+# Vivado(TM) HLS - High-Level Synthesis from C, C++ and SystemC v2018.3 (64-bit)
+# SW Build 2405991 on Thu Dec  6 23:36:41 MST 2018
+# IP Build 2404404 on Fri Dec  7 01:43:56 MST 2018
+# Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+# ==============================================================
+add_files main.c
+set_part xa7a12tcsg325-1q
+create_clock -name default -period 10
