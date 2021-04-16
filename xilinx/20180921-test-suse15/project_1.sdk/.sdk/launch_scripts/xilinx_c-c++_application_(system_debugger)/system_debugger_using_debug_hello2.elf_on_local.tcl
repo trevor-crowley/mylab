b54765397b782b35dcac3b48f154856426d61685
trevor-crowley <trevor_crowley@hotmail.com>
2021-04-16 11:32:23 -0400

    projet cleanup

--- xilinx/20180921-test-suse15/project_1.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_hello2.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183756705A"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183756705A"} -index 0
-dow /home/trevor/mylab/exp/20180921-test-suse15/project_1.sdk/hello2/Debug/hello2.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183756705A"} -index 0
-con
